200b4e2b9378687b1b64c6a31d415cb84fdc1175
todddrinkwater <daniel-reason>
2017-04-12 11:46:55 +1200

    setup boilerplate using sweet-as-beers

---
+++ client/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// This might need to be turned into a stateful component (aka container)
+const App = () => (
+  <div className='app'>
+  </div>
+)
+
+export default App
