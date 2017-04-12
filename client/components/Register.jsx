fbde69fd66f1af7c33a56d9144c1157c7430720b
daniel-reason <dreason@live.co.uk>
2017-04-12 18:31:50 +1200

    add register page form

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Register = () => (
-  <div>
-   Register Here
+  <div className='container'>
+    <div className='row'>
+      Sign Up!
+      <form action='#' method='get' id='register'>
+        <input type="text" placeholder="User Name" name='name'/><br />
+        <input type="text" placeholder="Email" name='email'/><br />
+        <input type="text" placeholder="Password" name='password'/><br />
+        <input type="text" placeholder="Confirm Password" name='password-confirm'/><br />
+        <button type='submit' form='register' value='Submit'>Register</button>
+      </form>
+    </div>
   </div>
 )
 
