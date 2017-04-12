34a4c340ce77da7c588365086771dd73cd2f2ddc
todddrinkwater <daniel-reason>
2017-04-12 16:24:27 +1200

    and main and profilebar components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Main from './Main'
 
 // This might need to be turned into a stateful component (aka container)
 const App = () => (
   <div className='app'>
-    Hello
+    <Main />
   </div>
 )
 
