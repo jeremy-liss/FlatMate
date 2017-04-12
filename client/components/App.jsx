0d98edda43133cfecae9b2afe1d0f980985deeab
eda013 <poukupenga@gmail.com>
2017-04-12 17:40:42 +1200

    added a roster to Components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 import Header from './Header'
 import ShoppingList from './shoppingList'
 import Main from './Main'
-
+import Roster from './Roster'
 
 // App function will need editing
 const App = () => (
+
   <div className='app'>
     <Header />
     <ShoppingList />
     <Main />
+    <Roster />
   </div>
 )
 
