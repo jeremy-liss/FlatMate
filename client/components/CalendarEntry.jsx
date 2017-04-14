f3e2e0d1bbb4df2b6d323901e3008ce141c03df4
daniel-reason <dreason@live.co.uk>
2017-04-14 19:16:56 +1200

    minor changes to calendar

--- client/components/CalendarEntry.jsx
+++ client/components/CalendarEntry.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 
 const CalendarEntry = (props) => (
   <div className='row'>
-    <form action='' method='get' id='calendar'>
-      <input type='text' palceholder='event'></input><br />
+    <form action='#/calendar' id='calendar'>
+      <input type='text' placeholder='New Event'></input>
+      <input type='date' name='event' min='{ todaysDate () }'></input>
       <button type='submit' form='calendar' value='Submit'>Add Event</button>
     </form>
   </div>
 )
+
+function todaysDate () {
+  var currentDate = new Date()
+  var day = currentDate.getDate()
+  var month = currentDate.getMonth() + 1
+  var year = currentDate.getFullYear()
+  return (year + "-" + month + "-" + day)
+}
 
 export default CalendarEntry
