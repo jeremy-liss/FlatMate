5266f1187e56575d018fe81e92b368a84f1b5dea
eda013 <poukupenga@gmail.com>
2017-04-16 14:32:13 +1200

    adding tilt.js

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -4,27 +4,55 @@ import ProfileBar from './ProfileBar'
 
 const Main = () => (
   <div className="container">
+
     <ProfileBar />
+
     <div className="row">
       <div className='six columns'>
-        <div className='shoppingList'>
-          <a href='#/shoppinglist'><img src="./../css/images/shoppingList.png" alt='link to shopping list' /></a>
+        <div className='shoppingList'
+            data-tilt data-tilt-scale="1.1">
+          <a href='#/shoppinglist'><img src="./../css/images/shoppingList.png" alt='link to shopping list' />
+          </a>
+
         </div>
+
       </div>
+
       <div className='six columns'>
-        <a href='#/calendar'><img src='./../css/images/calendar.png' alt='link to calendar' /></a>
+        <div className='shoppingList'
+            data-tilt data-tilt-scale="1.1">
+          <a href='#/calendar'><img src='./../css/images/calendar.png' alt='link to calendar' />
+          </a>
+
       </div>
+
     </div>
+
     <div className="row">
       <div className='six columns'>
-        <div className='roster'>
-          <a href='#/roster'><img src='./../css/images/roster.png' alt='link to roster' /></a>
+        <div className='roster'
+            data-tilt data-tilt-scale="1.1">
+          <a href='#/roster'><img src='./../css/images/roster.png' alt='link to roster' />
+          </a>
+
         </div>
+
       </div>
+
       <div className='six columns'>
-        <a href='#/bills'><img src='./../css/images/bills.png' alt='link to bills' /></a>
+        <div className='bills'
+            data-tilt data-tilt-scale="1.1">
+          <a href='#/bills'><img src='./../css/images/bills.png' alt='link to bills' />
+          </a>
+
       </div>
+
     </div>
+
+    <div>
+
+    </div>
+
   </div>
 )
 
