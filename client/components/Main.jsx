b6b9d92890059255db06f3838faa56fe6ed599d2
eda013 <poukupenga@gmail.com>
2017-04-17 11:48:47 +1200

    made changes

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,58 +1,29 @@
+
 import React from 'react'
 
 import ProfileBar from './ProfileBar'
 
 const Main = () => (
   <div className="container">
-
     <ProfileBar />
-
     <div className="row">
       <div className='six columns'>
-        <div className='shoppingList'
-            data-tilt data-tilt-scale="1.1">
-          <a href='#/shoppinglist'><img src="./../css/images/shoppingList.png" alt='link to shopping list' />
-          </a>
-
-        </div>
-
+        <a href='#/shoppinglist'><img src='../../css/images/shoppingList.png' alt='link to shopping list' /></a>
       </div>
-
       <div className='six columns'>
-        <div className='shoppingList'
-            data-tilt data-tilt-scale="1.1">
-          <a href='#/calendar'><img src='./../css/images/calendar.png' alt='link to calendar' />
-          </a>
-
+        <a href='#/calendar'><img
+        src='../../css/images/calendar11.png' alt='link to calendar'
+        /></a>
       </div>
-
     </div>
-
     <div className="row">
       <div className='six columns'>
-        <div className='roster'
-            data-tilt data-tilt-scale="1.1">
-          <a href='#/roster'><img src='./../css/images/roster.png' alt='link to roster' />
-          </a>
-
-        </div>
-
+        <a href='#/roster'><img src='../../css/images/roster.png' alt='link to roster' /></a>
       </div>
-
       <div className='six columns'>
-        <div className='bills'
-            data-tilt data-tilt-scale="1.1">
-          <a href='#/bills'><img src='./../css/images/bills.png' alt='link to bills' />
-          </a>
-
+        <a href='#/bills'><img src='../../css/images/bills.png' alt='link to bills' /></a>
       </div>
-
     </div>
-
-    <div>
-
-    </div>
-
   </div>
 )
 
