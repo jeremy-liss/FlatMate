0d1daff1d9dc9e4d007739ad070259c878147e3c
daniel-reason <dreason@live.co.uk>
2017-04-19 16:27:40 +1200

    remove unused bill_allocations migration

--- knexfile.js
+++ knexfile.js
@@ -29,8 +29,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection:
-  process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
