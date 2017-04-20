3f5810e4673c2f657f8f42ac195fc7eef93caeed
daniel-reason <dreason@live.co.uk>
2017-04-20 14:31:28 +1200

    refactored calendar functions

--- client/lib/eventDateOrganiser.js
+++ client/lib/eventDateOrganiser.js
@@ -3,36 +3,56 @@ module.exports = {
 }
 
 function dateOrganiser(events) {
-  
+
+  spruceAndSimplify (events) //Capitalizes Event name
+
+  filterEventDates (events)
+
+  sortEventDates (events)
+
+  correctDatesDisplay (events)
+
+  return events
+}
+
+
+function spruceAndSimplify (events) {
   if (events[0] != undefined) {
     events.map((day, i) => {
       events[i].event = day.event.charAt(0).toUpperCase() + day.event.slice(1)
       events[i].date = day.date.split('').filter(removeDash).join('')
-    })
-
-    events.map((day, i) => {
-      while (events.length > i) {
-        if (events[i].date < todaysDate()) {
-          events.splice(i, 1)
-        } else i++
-    }})
-
-    events.sort((a, b) => {
-      return a.date - b.date
-    })
-
-    events.map((day, i) => {
-      let a = day.date.split('')
-      let rearranged = [a[6],a[7],"-",a[4],a[5],"-",a[0],a[1],a[2],a[3]]
-      let joinedArray = rearranged.join('')
-      events[i].date = joinedArray
-    })
-  }
-    return events
+  })}
+  return events
 }
 
 function removeDash (value) {
   if (value !== "-") return value
+}
+
+function filterEventDates (events) {
+  events.map((day, i) => {
+    while (events.length > i) {
+      if (events[i].date < todaysDate()) {
+        events.splice(i, 1)
+      } else i++
+  }})
+  return events
+}
+
+function sortEventDates (events) {
+  events.sort((a, b) => {
+    return a.date - b.date
+  })
+  return events
+}
+
+function correctDatesDisplay (events) {
+  events.map((day, i) => {
+    let a = day.date.split('')
+    let rearranged = [a[6],a[7],"-",a[4],a[5],"-",a[0],a[1],a[2],a[3]]
+    let joinedArray = rearranged.join('')
+    events[i].date = joinedArray
+  })
 }
 
 function todaysDate () {
